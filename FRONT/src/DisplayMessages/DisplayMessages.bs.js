111ff6ddc65137d9186c6b4587f425d964840d9d
elise-lapauw <55782687+elise-lapauw@users.noreply.github.com>
2020-03-24 17:11:34 +0100

    Display List ok

--- FRONT/src/DisplayMessages/DisplayMessages.bs.js
+++ FRONT/src/DisplayMessages/DisplayMessages.bs.js
@@ -1,11 +1,24 @@
 'use strict';
 
+var React = require("react");
+var Belt_List = require("bs-platform/lib/js/belt_List.js");
 
 function DisplayMessages(Props) {
-  return /* () */0;
+  return Belt_List.toArray(Belt_List.map(/* :: */[
+                  1,
+                  /* :: */[
+                    2,
+                    /* :: */[
+                      3,
+                      /* [] */0
+                    ]
+                  ]
+                ], (function (num) {
+                    return React.createElement("div", undefined, String(num));
+                  })));
 }
 
 var make = DisplayMessages;
 
 exports.make = make;
-/* No side effect */
+/* react Not a pure module */
--- FRONT/src/DisplayMessages/DisplayMessages.re
+++ FRONT/src/DisplayMessages/DisplayMessages.re
@@ -2,25 +2,9 @@
 
 [@react.component]
 let make = _ => {
-    
-    let numbers = ["message 1", "Message 2", "Message 3"];
-    let listItems = numbers.map((number) =>
-      <li>{number}</li>
-    );
-    ReactDOM.render(<ul>{listItems}</ul>);
 
-  /* <div>
-        (ReasonReact.arrayToElement(
-          List.map(
-            num => <div>
-              (str(num ++ ": "))
-            </div>,
-            numbers
-          ) |> Array.of_list
-        ))
-      </div>
-      [1,2,3]
+      ([1,2,3])
     -> Belt.List.map( (num) => <div> {num -> string_of_int -> ReasonReact.string} </div>)
     -> Belt.List.toArray
-    -> ReasonReact.array */
+    -> ReasonReact.array
 };
